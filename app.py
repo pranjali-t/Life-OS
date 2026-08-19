8d43dd7e53c4a54c4026219568735db73f5a5ffd
pranjali-t <pranjalisingh164@gmail.com>
2026-08-20 02:40:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import os
-from google import genai
+from google.genai as genai
 
 # =====================================================
 # PAGE CONFIG
